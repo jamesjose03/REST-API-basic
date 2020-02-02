401cbee4bc14b2d8063ededf5035cf7c1723908d
James Kaviyil Jose <jamesjose03@gmail.com>
2020-02-02 12:13:37 +0530

    feat: GET request handling

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 
 const app = express()
+// res for response, req for request
+app.get('/',function(req,res){
+    console.log('GET REQUEST')
+    res.send({'name': 'James'})
+})
 
 app.listen(process.env.port||4000, function() {
     console.log('listening for requests')
